0f69263af4a14ba81ad1dede5bcca639e4c5fbc4
Ninja Hound <60533346+NinjaTG@users.noreply.github.com>
2021-04-14 11:38:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
 
             ist_now = datetime.datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%d/%m/%y %I:%M:%S %p")
 
-            edit_text += f"\n👉 <a href="https://t.me/MaxxBots/50">OTHERS BOTS 🌲</a>:\n\n🎯 𝙇𝙖𝙨𝙩 𝙪𝙥𝙙𝙖𝙩𝙚𝙙 & 𝙘𝙝𝙚𝙘𝙠𝙚𝙙 𝙤𝙣: \n\n__{str(ist_now)}__ 🇮🇳 IST\n__{utc_now}__ 🌎 UTC"
+            edit_text += f"\n👉 [🌲OTHERS BOTS 🌲](https://t.me/MaxxBots/50)\n\n🎯 𝙇𝙖𝙨𝙩 𝙪𝙥𝙙𝙖𝙩𝙚𝙙 & 𝙘𝙝𝙚𝙘𝙠𝙚𝙙 𝙤𝙣: \n\n__{str(ist_now)}__ 🇮🇳 IST\n__{utc_now}__ 🌎 UTC"
 
             user_client.edit_message_text(update_channel, status_message_id,
                                          edit_text)
